9f507d3a1938ef1123c3047d17961e7d9613394e
bruceskingle <bruce.skingle@symphony.com>
2018-08-16 06:59:01 +0100

    Auto remove docker container after run

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -u root --name fugue-jenkins -d -p 8080:8080 -p 50000:50000 -v jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock fugue/jenkins-aws:0.1
+docker run --rm -u root --name fugue-jenkins -d -p 8080:8080 -p 50000:50000 -v jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock fugue/jenkins-aws:0.1
